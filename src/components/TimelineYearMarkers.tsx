a461969aa02557cdeb90e613744ae8b1ec14d1ff
kai <addie60111@gmail.com>
2026-02-16 22:45:58 +0800

    feat: create TimelineYearMarkers component

---
+++ src/components/TimelineYearMarkers.tsx
@@ -0,0 +1,38 @@
+import { timeToPosition, type TimeRange } from '../utils/timeline'
+
+interface TimelineYearMarkersProps {
+  range: TimeRange
+}
+
+export default function TimelineYearMarkers({ range }: TimelineYearMarkersProps) {
+  // 生成年份標記
+  const startYear = parseInt(range.start.split('-')[0])
+  const endYear = parseInt(range.end.split('-')[0])
+
+  const years: number[] = []
+  for (let year = startYear; year <= endYear; year++) {
+    years.push(year)
+  }
+
+  return (
+    <div className="relative h-full">
+      {years.map(year => {
+        const yearDate = `${year}-01`
+        const position = timeToPosition(yearDate, range)
+
+        return (
+          <div
+            key={year}
+            className="absolute left-0 flex items-center"
+            style={{ top: `${position}%` }}
+          >
+            <span className="text-sm font-medium text-zinc-600 mr-2">
+              {year}
+            </span>
+            <div className="w-2 h-px bg-zinc-300" />
+          </div>
+        )
+      })}
+    </div>
+  )
+}
